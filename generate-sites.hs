c10a86bfe85cd84a718341c683ed9326cfa47f5a
phischu <pschuster@uni-koblenz.de>
2015-02-21 18:54:04 +0100

    use lucid

--- generate-sites.hs
+++ generate-sites.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Prelude hiding (writeFile)
+
+import Lucid (
+    renderText,Html,
+    html_)
+import Data.Text.Lazy.IO (writeFile)
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
     args <- getArgs
     print (length args)
-    writeFile "site/hello.html" "Hello World"
+    writeFile "site/hello.html" (renderText site)
+
+site :: Html ()
+site = html_ "Hallo World"
