0e57e3be878c717ce6dd20c22348810060587a34
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2017-08-24 11:36:47 +0200

    Serve slices for an environment.

--- src/FragnixServer.hs
+++ src/FragnixServer.hs
@@ -4,26 +4,41 @@
 module FragnixServer where
 
 
+import Fragnix.Slice (
+  Slice(Slice), SliceID, Use(Use), Reference(OtherSlice),
+  readSlice)
 import Fragnix.Environment (
   loadEnvironment)
+import Fragnix.SliceCompiler (
+  usedSliceIDs, sliceInstanceIDs)
+import Fragnix.DeclarationSlices (
+  moduleReference)
 import Servant (
-  Get, JSON, (:>), Capture,
+  Get, JSON, (:>), Capture, (:<|>)((:<|>)),
   serve, Server, Application)
 import Language.Haskell.Exts (
   ModuleName(ModuleName))
 import Language.Haskell.Names (
-  Environment, Symbol)
+  Environment, Symbol, symbolModule)
 
 import qualified Data.Map as Map (
   toList)
+import Control.Monad (
+  forM, unless, forM_)
 import Control.Monad.IO.Class (
   liftIO)
+import Control.Monad.Trans.State.Strict (
+  StateT,execStateT,get,put)
 import Data.Proxy (
   Proxy(Proxy))
 
 
+type API = EnvironmentAPI :<|> EnvironmentSlicesAPI
+
 application :: Application
-application = serve (Proxy :: Proxy EnvironmentAPI) serveEnvironment
+application = serve (Proxy :: Proxy API) (
+  serveEnvironment :<|> serveEnvironmentSlices)
+
 
 type EnvironmentAPI =
   "environment" :> Capture "environmentName" String :> Get '[JSON] [(String, [Symbol])]
@@ -37,4 +52,35 @@ serveEnvironment environmentName = do
 deconstructEnvironment :: Environment -> [(String, [Symbol])]
 deconstructEnvironment =
   map (\(ModuleName () moduleName, symbols) -> (moduleName, symbols)) . Map.toList
+
+
+type EnvironmentSlicesAPI =
+  "environment" :> Capture "environmentName" String :> "slices" :> Get '[JSON] [Slice]
+
+serveEnvironmentSlices :: Server EnvironmentSlicesAPI
+serveEnvironmentSlices environmentName = do
+  let environmentPath = "data/environments/" ++ environmentName
+  environment <- liftIO (loadEnvironment environmentPath)
+  let referencedSliceIDs = do
+        (_, symbols) <- Map.toList environment
+        symbol <- symbols
+        case moduleReference (symbolModule symbol) of
+          OtherSlice sliceID -> return sliceID
+          _ -> []
+  sliceIDs <- liftIO (execStateT (forM referencedSliceIDs loadSliceIDsStateful) [])
+  slices <- liftIO (forM sliceIDs (\sliceID -> do
+    readSlice ("data/slices/" ++ show sliceID)))
+  return slices
+
+
+loadSliceIDsStateful :: SliceID -> StateT [SliceID] IO ()
+loadSliceIDsStateful sliceID = do
+    seenSliceIDs <- get
+    unless (elem sliceID seenSliceIDs) (do
+        put (sliceID : seenSliceIDs)
+        slice <- liftIO (readSlice ("data/slices/" ++ show sliceID))
+        let recursiveSliceIDs = usedSliceIDs slice
+            recursiveInstanceSliceIDs = sliceInstanceIDs slice
+        forM_ recursiveSliceIDs loadSliceIDsStateful
+        forM_ recursiveInstanceSliceIDs loadSliceIDsStateful)
 
